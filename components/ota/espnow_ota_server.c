346d8fa609c47bfc360d1c29e84718896cf02de5
l1rn <ivtaks06@gmail.com>
2026-08-03 19:17:56 +0500

    change from client to server

--- components/ota/espnow_ota_server.c
+++ components/ota/espnow_ota_server.c
@@ -8,7 +8,7 @@
 #include <stdio.h>
 #include <string.h>
 
-static const char *TAG = "ESP_NOW_OTA_CLIENT";
+static const char *TAG = "ESP_NOW_OTA_SERVER";
 
 static u8 target_mac[6];
 static QueueHandle_t ack_queue = NULL;
@@ -18,44 +18,34 @@ typedef struct {
 	bool status_ok;
 } ack_response_t;
 
-static void send_ack(const u8 *dest_mac, u32 chunk_idx){
-	ota_packet_t ack_pkt = {
-		.type = OTA_CMD_ACK,
-		.chunk_idx = chunk_idx
-	};
-
-	if(!esp_now_is_peer_exist(dest_mac)){
-		esp_now_peer_info_t peer = {};
-		memcpy(peer.peer_addr, dest_mac, 6);
-		peer.channel = CONFIG_ESP_PEER_CHANNEL;
-		peer.encrypt = false;
-		esp_now_add_peer(&peer);
-	}
-
-	esp_now_send(dest_mac, (u8 *)&ack_pkt, sizeof(u8) + sizeof(u32));
-}
-
 #if ESP_IDF_VERSION >= ESP_IDF_VERSION_VAL(5, 1, 0)
 static void espnow_recv_cb(const esp_now_recv_info_t *recv_info, const u8 *data, int len){
-	u8 *src_mac = (u8 *) recv_info->src_addr;
-	int rssi = recv_info->rx_ctrl->rssi;
+	uint8_t *src_mac = (uint8_t *)recv_info->src_addr;
+	last_rssi = recv_info->rx_ctrl->rssi;
 #else
 static void espnow_recv_cb(const u8 *mac_addr, const u8 *data, int len){
-	u8 *src_mac = (u8 *) mac_addr;
-	int rssi = 0
+	uint8_t *src_mac = (uint8_t *)mac_addr;
 #endif
-	if(len >= sizeof(ota_packet_t) - OTA_CHUNK_SIZE){
+	if(len == sizeof(ota_packet_t)){
 		ota_packet_t *pkt = (ota_packet_t *)data;
-		if(pkt->type == OTA_CMD_ACK){
-			ESP_LOGI(TAG_CLIENT, "Chunk %lu/%lu received (Length: %d, Signal RSSI: %d dBm)",
-                        pkt->chunk_idx, pkt->total_chunks, pkt->data_len, rssi);
-
-			send_ack(src_mac, pkt->chunk_idx);
+		if(pkt->type == OTA_CMD_ACK && ack_queue != NULL){
+			ack_response_t resp = {
+				.acked_chunk = pkt->chunk_idx,
+				.status_ok = true
+			};
+			xQueueSendFromISR(ack_queue, &resp, NULL);
 		}
 	}
 }
 
-esp_err_t espnow_ota_client_init(const u8 *recv_mac){
+esp_err_t espnow_ota_server_init(const u8 *recv_mac){
+	memcpy(target_mac, recv_mac, 6);
+
+	ack_queue = xQueueCreate(5, sizeof(ack_response_t));
+	if(ack_queue == NULL){
+		ESP_LOGE(TAG, "Failed to create ACK channel");
+		return ESP_FAIL;
+	}
 
 	ESP_ERROR_CHECK(esp_now_init());
 	ESP_ERROR_CHECK(esp_now_register_recv_cb(espnow_recv_cb));
--- components/process_manager/process_manager.c
+++ components/process_manager/process_manager.c
@@ -1,13 +1,18 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 
-#include "uart_component.h"
+#include "uart_component.h" 
 #include "ota.h"
+#include "espnow_ota_server.h"
 #include "http_server.h"
 #include "ring_buffer.h"
 #include "temperature_sensor.h"
 #include "esp_http_server.h"
 #include "wifi_component.h"
+
+typedef uint8_t u8;
+
+static u8 main_mac[6] = { 0x80, 0xF3, 0xDA, 0xAC, 0x4C, 0x04 };
 
 void create_main_loop(){
 #if CONFIG_ESP_ENABLE_UART
@@ -18,7 +23,6 @@ void create_main_loop(){
 #endif // CONFIG_ESP_ENABLE_WEB_LOGS
 	init_temperature_config();
 	start_softap_sta();
-	
 
 #if CONFIG_ESP_ENABLE_UART
 	xTaskCreate(tcp_server_task, "tcp_server", 4096, NULL, 5, NULL);
@@ -29,6 +33,7 @@ void create_main_loop(){
 		if(wifi_is_connected() && !is_connected){
 			httpd_handle_t s = start_server();
 			register_ota_uri(s);
+			espnow_ota_server_init(main_mac);
 			is_connected = true;
 		} else {
 			is_connected = false;
